2ee9f3ed17f8a6dcc73cf9eaba74a24408785e02
Mateo <agusmastex@gmail.com>
2023-03-16 18:01:18 -0300

    Succed in solving the steady state using ODE solvers
    Primera resolución del problema estacionario de flujo compresible, introduciendo la ecuación de estado (gas ideal) analíticamente y resumiendo en un sistema matricial. Método numérico: ODE-like solver, DifferentialEquations.jl, Rodas4

--- single-phase/Compressible/steady (matrix).jl
+++ single-phase/Compressible/steady (matrix).jl
@@ -1,10 +1,10 @@
 using Plots
-# using LinearAlgebra
+using LinearAlgebra
 using DifferentialEquations
 
-Q  = 1.0  		    # J/s
+Q  = 1000.0         # J/s
 Cp = 1996.0  		# J/kg K
-R  = 1.0/1000		# m
+R  = 5.0/100		# m
 A  = π*R^2   		# m^2
 L  = 10.0    	 	# m                
 Lh = 0.5*L   		# m
@@ -15,25 +15,43 @@ g  = 9.8		 	# m/sꜝ
 f  = 3.0    	    # 1
 Δp = 0.0 	       	# Pa
 
-ρ0 = 0.60 			# kg/m³
-v0 = 3.0			# m/s
-m  = ρ0*v0			# kg/s
+# Initial conditions
 P0 = 1e5
-M  = 18e-3
+v0 = 10.0			# m/s
+
+M  = 18e-3          # kg/mol
 Rg = 8.314
 Cp = Cp*M           # J/mol K
 Cv = Cp - Rg        # J/mol K
+ρ0 = P0*M/(Rg*T0) 			# kg/m³
+m  = ρ0*v0			# kg/s
 
+# Setup
 Q_hat(z) = z < Lh ? Q/(A*Lh) : 0
-
 S = [0 0; -0.025f*m/R 0]
 
 function F(X,p,z)
     M = [Cp/Rg*X[2] Cv/Rg*X[1]; m 1]
     b = [Q_hat(z); 0]
-    return M/(b+S*X)
+    return M\(b+S*X)
 end
 
+# Solve
 prob = ODEProblem(F,[v0,P0],[0,L])
-sol  = solve(prob)
-plot(sol, layout=(2,1))
+sol  = solve(prob, Rodas4())
+
+# Post-process
+z = 0:0.01:L
+v = sol(z)[1,:]
+P = sol(z)[2,:]
+T = M/(m*Rg) * (P.*v)
+
+P = 1e-5*P
+T = T .- 273.15
+
+plot_1 = plot(z,T, title="T")
+plot_2 = plot(z,P, title="P")
+plot_3 = plot(z,v, title="Vz")
+
+
+final_plot = plot(plot_1, plot_2, plot_3, layout=(3,1))
